1e62146dd930375496d25daff5cc9688622accc7
Ricardo <jesus.pelaez@ucb.edu.bo>
2022-11-18 00:27:28 -0400

    establecer conexion con la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PiedrasModule } from './piedras/piedras.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PiedrasModule],
+  imports: [
+    PiedrasModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'piedras-preciosas',
+      synchronize: false,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
